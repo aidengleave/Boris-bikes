5ad953051279235c066646560f382e1f5189e7f5
Aiden Gleave <aidengleave@gmail.com>
2019-01-09 14:27:37 +0000

    Step 8 refactor to one line

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-    it 'releases bike' do 
-        docking_station = DockingStation.new
-    expect(docking_station).to respond_to('release_bike')
-    end    
+    it {should respond_to(:release_bike)}   
 end
