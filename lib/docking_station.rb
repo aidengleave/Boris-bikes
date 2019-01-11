384ce191d97ef05ebcb8f707c969f06f3c127b0b
Aiden Gleave <aidengleave@gmail.com>
2019-01-11 14:18:01 +0000

    Step 13: Added test error for too many bikes in dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,17 @@
 require_relative 'bike'
-
+# docks and releases bikes to users
 class DockingStation
-
   attr_reader :bike
 
-  #  def initialize
-  #    @bike = Bike.new
-  #  end
-
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike 
+    raise 'No bikes available' unless @bike
+
+    @bike
   end
 
   def dock(bike)
+    raise 'Docking station full' if @bike
+
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,41 +1,33 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-
-  it { is_expected.to respond_to :release_bike }
-  # * a method within a class becomes and object hence "respond_to" checks for the object (:release_bike)
-  it "Gets a bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to (be_an_instance_of(Bike))
-  end
- 
-  it "bike is working" do
+  it 'bike is working' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike.working?). to be true
   end
- 
-  it "docks something" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
- 
-  describe ' #release_bike' do
- 
-    it "returns docked bikes" do
+
+  describe '#release_bike' do
+    it 'releases docked bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
- 
+
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
- 
-  # it "no infinite bikes" do
-  #   expect { raise subject.release_bike }.to raise_error
-  # end
- 
- end
+
+  describe '#dock' do
+    it 'docks bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+end
